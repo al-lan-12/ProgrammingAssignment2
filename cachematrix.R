b9e07da579febb768148f3cc6db332ff06177c1d
Allan Joe <60976221+al-lan-12@users.noreply.github.com>
2020-05-30 10:58:52 +0530

    Assignment Submission

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,51 @@
 
 ## Write a short comment describing this function
 
+##This functioncerates matrix that caches itself
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv<-NULL
+  
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  
+  get<-function(){
+    x
+  }
+  
+  setinverse<-function(solve.Matrix){
+    inv<<-solve.Matrix
+  }
+  
+  getinverse<-function(){
+    inv
+  }
+  
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 
 }
 
 
 ## Write a short comment describing this function
 
+##This function gives inverse of the matrix returned the above function
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv<-x$getinverse()
+  
+  if(!is.null(inv)){
+    message("getting from cache")
+    return(inv)
+    }
+  
+  data<-x$get()
+  
+  inv<-solve(data)
+  
+  x$setinverse(inv)
+ 
+   inv
 }
